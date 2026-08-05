a477bee8cda48bc48e7d1b959c1f80eb2557a443
Nova <novadevv12333@gmail.com>
2026-08-05 21:23:43 +0930

    Update reload.lua

--- reload.lua
+++ reload.lua
@@ -1,11 +1,17 @@
-if fs.exists("mfe-reader.lua") then
-    fs.delete("mfe-reader.lua")
+local url = "https://raw.githubusercontent.com/NovaDevvvv/computercraft-scripts/refs/heads/main/mfe-reader.lua"
+local file = "mfe-reader.lua"
+
+if fs.exists(file) then
+    fs.delete(file)
 end
 
-shell.run(
-    "wget",
-    "https://raw.githubusercontent.com/NovaDevvvv/computercraft-scripts/refs/heads/main/mfe-reader.lua",
-    "mfe-reader.lua"
-)
+print("Downloading " .. file .. "...")
 
-shell.run("mfe-reader.lua")
+local success = shell.run("wget", url, file)
+
+if not success or not fs.exists(file) then
+    error("Failed to download " .. file)
+end
+
+print("Starting " .. file .. "...")
+shell.run(file)
